c57799527440a7dcb0c509c186abdd8503a55cf6
atulprakash18 <112651419+atulprakash18@users.noreply.github.com>
2022-10-28 22:40:07 -0700

    Initial Commit

---
+++ Urinals/urinal.java
@@ -0,0 +1,18 @@
+
+/**
+ * @author Atul Prakash
+ */
+
+
+public class urinals {
+
+    Boolean answer = goodString("test");
+
+    private Boolean goodString(String str) {
+        System.out.println("Not yet implemented");
+        return true;
+    }
+
+    System.out.println("Function complete");
+};
+
